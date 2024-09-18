307ad6d61d55fc71042468d1e8fc9ff23f6795a9
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2024-09-18 09:37:46 -0400

    added challange

---
+++ javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -0,0 +1,3 @@
+console.log(100); 
+console.log('hello');
+console.log('Oscar')
--- javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
+++ javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,16 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+
+let firstLetter;
+let neWString; 
+
+
+firstLetter = myString.charAt(0).toUpperCase()
+
+newString = firstLetter + myString.slice(1, 9); 
+
+
+
+
+console.log(newString);
+
