0c4e980ee9ee766ac908f76291f6e594daadecab
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-04 16:50:18 -0400

    added for-loop examples

--- javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
+++ javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
@@ -5,3 +5,18 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+
+// Loop through an array 
+
+const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim']; 
+
+for(let i = 0; i < names.length; i++) {
+  if (names[i] === 'Sara') {
+    console.log(`${names[i]} is the best!`)
+  } else {
+    console.log(names[i]); 
+  }
+
+  
+} 
