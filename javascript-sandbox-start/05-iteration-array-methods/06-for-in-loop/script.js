4e190860cc6011f8f9dcc7abb02b8b11345c6f1f
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-05 16:13:41 -0400

    added for in loop

---
+++ javascript-sandbox-start/05-iteration-array-methods/06-for-in-loop/script.js
@@ -0,0 +1,15 @@
+// For in Loop 
+
+const colorObj = {
+  color1: 'red',
+  color2: 'blue', 
+  color3: 'orange', 
+  color4: 'green',
+
+
+}; 
+
+
+for (const key in colorObj) {
+  console.log(key); 
+}
