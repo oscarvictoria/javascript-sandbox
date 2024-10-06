45b15865b3bb557d7d281f4158e09d1f64abb313
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-06 17:09:45 -0400

    added array high order functions

---
+++ javascript-sandbox-start/05-iteration-array-methods/08-filter/script.js
@@ -0,0 +1,60 @@
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+// Filter even numbers using filter method 
+
+const evenNumbers = numbers.filter(function(number) {
+  return number % 2 === 0; 
+})
+
+// console.log(evenNumbers); 
+
+
+// Using For Each method, display only even numbers 
+
+
+
+numbers.forEach(function(item) {
+  let x =[]; 
+
+  if (item % 2 === 0) {
+    x.push(item)
+  }
+
+ 
+
+})
+
+const students = [
+  { name: "Alice", age: 20, major: "Computer Science" },
+  { name: "Bob", age: 22, major: "Mathematics" },
+  { name: "Charlie", age: 21, major: "History" }
+];
+
+
+// Display only student s 21 and over
+
+const overAge = students.filter(function (student) {
+  return student.age >= 21; 
+
+}) 
+
+for(let i = 0; i <= students.length; i++) {
+  console.log(students.name); 
+}
+
+
+let newArr = []; 
+
+for (let i = 0; i <= overAge.length - 1; i++) {
+  newArr.push(overAge[i].name); 
+
+}
+
+console.log(newArr); 
+
+
+
+
+
+// Expected result -[ 'Bob, 'Charlie ] 
+
