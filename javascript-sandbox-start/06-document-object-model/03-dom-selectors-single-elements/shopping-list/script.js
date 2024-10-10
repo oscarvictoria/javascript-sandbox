2c6141b76c72d6880488eee3096b32cfb236998d
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-10 17:07:07 -0400

    Added a for each array method to change the name of on of the items

--- javascript-sandbox-start/06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
@@ -11,20 +11,41 @@ header.textContent = 'Grocery List';
 
 let form = document.querySelector('#item-input');
 
+let placeHolder = form.attributes[4]; 
+
+placeHolder.textContent = 'Enter Item Here'; 
+
 
 
 let button = document.querySelector('.btn')
 button.textContent =  "+"
 
-let itemList = document.querySelector('#item-list').children; 
+let itemList = document.querySelector('#item-list').querySelectorAll('li')
+
 
 itemList[0].innerHTML =  `Mango
 <button class="remove-item btn-link text-red">
   <i class="fa-solid fa-xmark"></i>
 </button>`
 
-let placeHolder = form.attributes[4]; 
-
-placeHolder.textContent = 'Enter Item Here'; 
 
 
+
+// Use an array method to change Milk to Almond Milk 
+
+console.log(itemList)
+
+itemList.forEach(function(item) {
+
+  if (item.innerText === 'Milk') {
+
+    item.innerHTML = `Almond Milk
+    <button class="remove-item btn-link text-red">
+      <i class="fa-solid fa-xmark"></i>
+    </button>`
+
+  }
+  
+
+
+})
