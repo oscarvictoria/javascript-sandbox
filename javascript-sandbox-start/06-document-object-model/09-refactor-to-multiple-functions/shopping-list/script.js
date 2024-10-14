c94973fac8c0cacf5034d01d9f542d68b6e74209
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-13 23:00:04 -0400

    added a function to add items using HTML

--- javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -15,3 +15,18 @@ function createNewItem(item) {
 }
 
 createNewItem('Cheese');
+
+
+// Using HTML 
+
+function createItem(item) {
+  const li = document.createElement('li'); 
+  li.innerHTML = `${item}
+  <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`
+  document.querySelector('.items').appendChild(li); 
+}
+
+
+createItem('Chicken');
