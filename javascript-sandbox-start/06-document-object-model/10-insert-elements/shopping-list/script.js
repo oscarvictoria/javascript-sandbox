cb12b842f9512f55cb2542e82358ea3db00b4267
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-13 23:22:18 -0400

    added a way to add items using insertAdjacentElement method

--- javascript-sandbox-start/06-document-object-model/10-insert-elements/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/10-insert-elements/shopping-list/script.js
@@ -1,5 +1,17 @@
 // insertAdjacentElement Example
 
+let newItem = document.createElement('li')
+newItem.innerHTML = `Lettuce
+<button class="remove-item btn-link text-red">
+  <i class="fa-solid fa-xmark"></i>
+</button>`
+
+let list = document.querySelector('.items'); 
+
+list.insertAdjacentElement('beforeend', newItem);
+
+
+
 // insertAdjacentText Example
 
 // insertAdjacentHTML example
