f7d7a7521296b72ac8f5c1e65f8a7fbae5b22315
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-10-16 22:15:55 -0400

    added array challenges

---
+++ javascript-sandbox-start/02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,8 @@
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+
+// reverseArray(sentence); 
+// Should return ['This', 'will', 'all', 'make', 'sense.'];
+
+let newArr = []
+
+console.log('Hello ')
---
+++ javascript-sandbox-start/02-arrays-and-objects/Challenges/script.js
@@ -0,0 +1,12 @@
+/*
+
+Write a function, reverseArray(), that takes in an array as an argument and 
+returns a new array with the elements in the reverse order.
+
+There’s a built-in array method called .reverse() that does a lot of this work for you, 
+but you’re NOT allowed to use it here!
+
+*/ 
+
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+
