6d0b74770c3e2f8a78b479099cf4c7abaa930e2e
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2024-11-05 14:13:08 -0500

    completed project using innerHTML to hardcode the values of the key events in the page

--- javascript-sandbox-start/07-events/05-event-keycode-project/event-keycodes/script.js
+++ javascript-sandbox-start/07-events/05-event-keycode-project/event-keycodes/script.js
@@ -9,13 +9,27 @@ const keyClicked = document.querySelector('body');
 // Get the key clicked 
 
 function getKey(e) {
-    key.forEach( function(key){
-        key.innerHTML = e.key; 
+    key.forEach( function(key, index){
+
+    if (index === 0) {
+        key.innerHTML = `<div class="key">
+        ${e.key}
+        <small>e.key</small>
+      </div>`
+    } else if (index === 1) {
+        key.innerHTML = `<div class="key">
+        ${e.keyCode}
+        <small>e.keyCode</small>
+      </div>`
+    } else if (index === 2) {
+        key.innerHTML = `<div class="key">
+        ${e.code}
+        <small>event.code</small>
+      </div>`
+    }
         
     })
-    
- 
-    
+       
 }
 
 keyClicked.addEventListener('keypress', getKey)
