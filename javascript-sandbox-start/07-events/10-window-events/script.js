98a0ebfeb91fba6c12de85af147e545609cb890b
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2024-11-17 20:59:03 -0500

    added window events

--- javascript-sandbox-start/07-events/10-window-events/script.js
+++ javascript-sandbox-start/07-events/10-window-events/script.js
@@ -1,1 +1,17 @@
 
+window.onload = function() {
+  document.querySelector('h1').textContent = 'Hello'
+}
+
+function execute(e) {
+  console.log('window resized ' +  e.target.innerWidth)
+
+}
+
+function copy() {
+  console.log('Data was copied')
+}
+
+window.addEventListener('resize', execute) 
+
+window.addEventListener('copy', copy)
