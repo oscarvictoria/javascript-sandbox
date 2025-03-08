0250bee0de6f660e71e330cdabc49360119cb3b7
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-03-07 21:06:13 -0500

    review of array nesting, concat, and spread

---
+++ javascript-sandbox-start/02-arrays-and-objects/03-array-nesting-concat-spread/script.js
@@ -0,0 +1,40 @@
+let x; 
+
+// Array Nesting 
+let fruits = ['organge', 'apple'] 
+
+let berries = ['strawberries', 'blueberries'];
+
+// fruits.push(berries);
+
+
+x = fruits;
+
+x = fruits[1];
+
+
+
+// Array concat
+
+// x = fruits.concat(berries);
+
+
+
+
+
+
+
+// Array spread 
+
+// x = [...fruits, ...berries];
+
+
+
+
+// Static Array methods
+
+
+
+
+
+console.log(x); 
