8528348f0b69dacf8b9c29aed321ad7abcccbcba
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-04 03:24:57 -0400

    added methods to change the color of the items in the shopping list using querySlectorAll

--- javascript-sandbox-start/06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
@@ -1,3 +1,23 @@
 let items = document.querySelectorAll('.item'); 
 
 console.log(items[3].innerText)
+
+// Change the color of the items using querySelectorAll 
+ let firstItem = items[0]; 
+ let secondItem = items[1]; 
+ let thirdItem = items[2];
+
+ let newItem = 'Grapes'
+
+
+ firstItem.style.color = 'red'
+ secondItem.style.backgroundColor = 'orange'
+
+ thirdItem.innerHTML = ` ${newItem}
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`
+
+thirdItem.style.backgroundColor = 'purple'
+thirdItem.style.color = 'white'
+
