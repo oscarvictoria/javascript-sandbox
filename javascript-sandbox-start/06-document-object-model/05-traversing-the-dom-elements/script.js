4d4abdd5852e8c989b50159ccbd55ecb0a85f2b9
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-05 04:21:39 -0400

    addedd properties to traverse the DOM

--- javascript-sandbox-start/06-document-object-model/05-traversing-the-dom-elements/script.js
+++ javascript-sandbox-start/06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -4,8 +4,26 @@ let parent = document.querySelector('.parent')
 
 // Get the children 
 
-let childern = parent.children; 
-console.log(childern[0].innerText)
+let children = parent.children; 
+console.log(children[0].innerText)
 
 // Get parent from child element
+let child = document.querySelector('.child'); 
+
+output = child; 
+
+output = child.parentElement; 
+
+
+// Get the next element by using getNextElementSibling property 
+
+let secondElement = children[1]; 
+
+let thirdElement = secondElement.nextElementSibling.innerText;
+
+let firstItem = secondElement.previousElementSibling.innerText; 
+
+output = firstItem; 
+
+console.log(output); 
 
