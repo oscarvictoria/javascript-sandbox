59e5bae84f0840f5491d2a6adf46ac894849c92a
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-05 12:53:53 -0400

    added methods to traverse DOM nodes

---
+++ javascript-sandbox-start/06-document-object-model/06-traversing-the-dom-nodes/script.js
@@ -0,0 +1,34 @@
+let output;
+
+const parent = document.querySelector('.parent'); 
+
+output = parent.childNodes; 
+
+output = parent.childNodes[0]; // Space 
+output = parent.childNodes[1]; // Comment 
+output = parent.childNodes[2]; // Space 
+output = parent.childNodes[3];  // First Node 
+
+let textNode = parent.childNodes[1]; 
+
+output = textNode.textContent;
+
+let firstChild = parent.childNodes[3]; 
+
+output = firstChild.textContent;
+
+firstChild.style.color = 'red';
+
+output = parent.childNodes; 
+
+
+
+
+
+
+
+
+
+
+
+console.log(output); 
