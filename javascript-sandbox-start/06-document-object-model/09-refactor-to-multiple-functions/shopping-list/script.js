45a1cafe4ca947a5bf223ef55c1dd9f55be98377
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-06 02:21:34 -0400

    added two functions to create the button and icon

--- javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -2,20 +2,38 @@ function createNewItem(item) {
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(item));
 
-  const button = document.createElement('button');
-  button.className = 'remove-item btn-link text-red';
+  let button = createButton('remove-item btn-link text-red')
 
-  const icon = document.createElement('i');
-  icon.className = 'fa-solid fa-xmark';
+  li.appendChild(button)
 
-  button.appendChild(icon);
-  li.appendChild(button);
+
 
   document.querySelector('.items').appendChild(li);
 }
 
-createNewItem('Cheese');
 
+
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes; 
+
+  let icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  return button
+
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+
+}
+
+createNewItem('Chips');
+
+ 
 
 // Using HTML 
 
@@ -30,3 +48,5 @@ function createItem(item) {
 
 
 createItem('Chicken');
+
+createNewItem('Ice');
