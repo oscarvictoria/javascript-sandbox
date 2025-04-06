d413929654df85a128f9b17e5015ec7ec29c57de
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-06 01:18:55 -0400

    added a function to create new items and add them to the shopping list

--- javascript-sandbox-start/06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/script.js
@@ -1,15 +1,15 @@
-
+// Using innerHTML 
 
 let button = document.querySelector('.btn')
 
 function add() {
   let item = document.createElement('li')
-  item.innerHTML = `Eggs
-  <button class="remove-item btn-link text-red">
+ item.innerHTML = `Eggs
+  <button class="remove product btn-link text-red">
     <i class="fa-solid fa-xmark"></i>
   </button>`
 
-  document.body.querySelector('.items').appendChild(item)
+  document.body.querySelector('.items').appendChild(item);
 
 
 
@@ -20,3 +20,39 @@ function add() {
 add()
 
 button.addEventListener("click", add);
+
+// Using createElement method 
+
+
+function createItem(product) {
+  // Creates the element
+  let newProduct = document.createElement('li') 
+  newProduct.appendChild(document.createTextNode(product));
+  
+
+  // Adds to the list
+  document.querySelector('.items').appendChild(newProduct); 
+
+  let list = document.querySelector('.items'); 
+
+  // Creates the button 
+  let button = document.createElement('button'); 
+  button.className = 'remove-item btn-link text-red'
+  
+ // Adds button to the li
+  newProduct.appendChild(button);
+
+
+  // creates icon 
+  let icon = document.createElement('i'); 
+  icon.className = 'fa-solid fa-xmark'
+
+  // Adds icon to the button 
+  button.appendChild(icon)
+
+  
+}
+
+
+createItem('Beans');
+createItem('rice')
