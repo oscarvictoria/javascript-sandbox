5063593ccf98387620232702295464d7b0602ce6
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-12 12:19:58 -0400

    added styles

--- javascript-sandbox-start/06-document-object-model/14-styles-classes/script.js
+++ javascript-sandbox-start/06-document-object-model/14-styles-classes/script.js
@@ -3,7 +3,7 @@ let text = document.querySelector('p');
 let list = document.querySelector('ul'); 
 
 function buttonClicked() {
-    text.classList.add('dark'); 
+    text.classList.add('dark'); // Adds a class
 }
 
 button.onclick = buttonClicked; 
