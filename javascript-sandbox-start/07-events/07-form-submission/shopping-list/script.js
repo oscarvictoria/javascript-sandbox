81a2da404fc34ff1ca54e33328e1270db1b7be7a
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-04-15 23:59:32 -0400

    added formData Object

--- javascript-sandbox-start/07-events/07-form-submission/shopping-list/script.js
+++ javascript-sandbox-start/07-events/07-form-submission/shopping-list/script.js
@@ -4,8 +4,18 @@ function getValues(e) {
 const item = document.querySelector('.form-input')
 const priority = document.querySelector('#priority-input');
 e.preventDefault()
-console.log(item.value, priority.value);
+// console.log(item.value, priority.value);
 
 }
 
 itemForm.addEventListener('submit', getValues); 
+
+
+function getData () {
+  
+    const formData = new FormData(itemForm); 
+    console.log(formData.name);
+    
+}
+
+getData()
