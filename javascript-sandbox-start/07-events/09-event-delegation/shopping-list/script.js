8b083c4941f0a5f5856a38cd358bfdf69d974e14
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-05-12 23:46:40 -0400

    added evemt de;agation method

--- javascript-sandbox-start/07-events/09-event-delegation/shopping-list/script.js
+++ javascript-sandbox-start/07-events/09-event-delegation/shopping-list/script.js
@@ -1,25 +1,14 @@
-const button = document.querySelector('.btn'); 
-const div = document.querySelector('form div:nth-child(2)'); 
+const ul = document.querySelector('ul'); 
 
-function buttonClicked(e) {
-    e.stopPropagation()
-    alert('button clicked')
-    
+function itemClicked(e) {
+    console.log(e.target)
+    if (e.target.tagName === 'LI'){
+     e.target.style.color = 'red'
+    }
+
+
+
 }
 
-function divClicked(e) {
-     alert('div clicked')
-    
-}
 
-button.addEventListener('click', buttonClicked); 
-
-div.addEventListener('click', divClicked); 
-
-
-// When we have a parent using an event listener the parent event also get's called when the child element is clicked
-
-// To avoid this, we can add the e.stopPropagation method on the event of the chosen child element 
-
-
-
+ul.addEventListener('click', itemClicked); 
