a862a6599758a158737b458ada15b36a3ce8cb0f
Oscar Victoria <oscarvictoriagonzalez@gmail.com>
2025-05-12 23:19:53 -0400

    updated file to match the correct folder

---
+++ javascript-sandbox-start/07-events/08-event-bubbling/shopping-list/script.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('.btn'); 
+const div = document.querySelector('form div:nth-child(2)'); 
+
+function buttonClicked(e) {
+    e.stopPropagation()
+    alert('button clicked')
+    
+}
+
+function divClicked(e) {
+     alert('div clicked')
+    
+}
+
+button.addEventListener('click', buttonClicked); 
+
+div.addEventListener('click', divClicked); 
+
+
+// When we have a parent using an event listener the parent event also get's called when the child element is clicked
+
+// To avoid this, we can add the e.stopPropagation method on the event of the chosen child element 
